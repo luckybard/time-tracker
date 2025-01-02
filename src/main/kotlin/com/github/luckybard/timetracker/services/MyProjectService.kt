b2ac5e6fbf81c87959f68493ddd69470efb45cb5
GitHub Action <action@github.com>
2025-01-02 20:35:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/luckybard/timetracker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/luckybard/timetracker/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.luckybard.timetracker.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.luckybard.timetracker.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
