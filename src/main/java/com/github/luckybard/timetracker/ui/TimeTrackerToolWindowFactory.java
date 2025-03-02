ca933139e525b2cca435704a2e7adaef60e87c1f
bartoszmucha <bartosz.mucha@outlook.com>
2025-03-02 21:22:59 +0100

    [TT-icon] Updated plugin icon

--- src/main/java/com/github/luckybard/timetracker/ui/TimeTrackerToolWindowFactory.java
+++ src/main/java/com/github/luckybard/timetracker/ui/TimeTrackerToolWindowFactory.java
@@ -2,6 +2,7 @@ package com.github.luckybard.timetracker.ui;
 
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
@@ -16,5 +17,10 @@ public class TimeTrackerToolWindowFactory implements ToolWindowFactory, DumbAwar
         ContentFactory contentFactory = ContentFactory.getInstance();
         Content content = contentFactory.createContent(panel.getContent(), "", false);
         toolWindow.getContentManager().addContent(content);
+    }
+
+    @Override
+    public void init(ToolWindow toolWindow) {
+        toolWindow.setIcon(IconLoader.getIcon("/icons/logo.svg", getClass()));
     }
 }
