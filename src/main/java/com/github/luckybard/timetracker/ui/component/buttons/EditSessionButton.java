a29e1a00d4db520c89314d107b407912ba902938
Bartosz Mucha <bartosz.mucha@outlook.com>
2025-03-08 20:31:25 +0100

    [TT-19] Added edit session validation

--- src/main/java/com/github/luckybard/timetracker/ui/component/buttons/EditSessionButton.java
+++ src/main/java/com/github/luckybard/timetracker/ui/component/buttons/EditSessionButton.java
@@ -2,9 +2,13 @@ package com.github.luckybard.timetracker.ui.component.buttons;
 
 import com.github.luckybard.timetracker.model.Session;
 import com.github.luckybard.timetracker.service.SessionService;
+import com.github.luckybard.timetracker.util.TimeUtils;
 
 import javax.swing.*;
 import java.awt.*;
+
+import static com.github.luckybard.timetracker.util.TimeUtils.isEndTimeBeforeStartTime;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class EditSessionButton extends ColumnButtonEditor {
     public EditSessionButton(SessionService sessionService, Runnable reloadTable) {
@@ -24,21 +28,53 @@ public class EditSessionButton extends ColumnButtonEditor {
             JPanel panel = new JPanel(new GridLayout(4, 2));
             panel.add(new JLabel("Branch:"));
             panel.add(branchField);
-            panel.add(new JLabel("Date:"));
+            panel.add(new JLabel("Date (yyyy-MM-dd):"));
             panel.add(dateField);
-            panel.add(new JLabel("Start Time:"));
+            panel.add(new JLabel("Start Time (HH:mm):"));
             panel.add(startTimeField);
-            panel.add(new JLabel("End Time:"));
+            panel.add(new JLabel("End Time (HH:mm):"));
             panel.add(endTimeField);
 
             int result = JOptionPane.showConfirmDialog(null, panel, "Edit Session", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
             if (result == JOptionPane.OK_OPTION) {
-                session.setBranch(branchField.getText());
-                session.setDate(dateField.getText());
-                session.setStartTime(startTimeField.getText());
-                session.setEndTime(endTimeField.getText());
-                getReloadTable().run();
+                boolean shouldSave = true;
+                String branch = branchField.getText();
+                String date = dateField.getText();
+                String startTime = startTimeField.getText();
+                String endTime = endTimeField.getText();
+
+                if (isEmpty(branch) || isEmpty(date) || isEmpty(startTime) || isEmpty(endTime)) {
+                    shouldSave = false;
+                    JOptionPane.showMessageDialog(null, "All fields must be filled out.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                    this.handleButtonClick(row);
+                }
+
+                if (!TimeUtils.isValidDate(date)) {
+                    shouldSave = false;
+                    JOptionPane.showMessageDialog(null, "Invalid date format. Please use yyyy-MM-dd.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                    this.handleButtonClick(row);
+                }
+
+                if (!TimeUtils.isValidTime(startTime) || !TimeUtils.isValidTime(endTime)) {
+                    shouldSave = false;
+                    JOptionPane.showMessageDialog(null, "Invalid time format. Please use HH:mm.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                    this.handleButtonClick(row);
+                }
+
+                if (isEndTimeBeforeStartTime(startTime, endTime)) {
+                    shouldSave = false;
+                    JOptionPane.showMessageDialog(null, "Invalid time. End time cannot be before start time", "Input Error", JOptionPane.ERROR_MESSAGE);
+                    this.handleButtonClick(row);
+                }
+
+                if(shouldSave){
+                    session.setBranch(branch);
+                    session.setDate(date);
+                    session.setStartTime(startTime);
+                    session.setEndTime(endTime);
+                    getReloadTable().run();
+                }
             }
         }
     }
---
+++ src/main/java/com/github/luckybard/timetracker/util/TimeUtils.java
@@ -0,0 +1,46 @@
+package com.github.luckybard.timetracker.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtils {
+
+    public static boolean isValidDate(String date) {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidTime(String time) {
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        timeFormat.setLenient(false);
+        try {
+            timeFormat.parse(time);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isEndTimeBeforeStartTime(String startTime, String endTime) {
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        timeFormat.setLenient(false);
+
+        try {
+            Date formattedStartTime = timeFormat.parse(startTime);
+            Date formattedEndTime = timeFormat.parse(endTime);
+            return formattedEndTime.before(formattedStartTime);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
